5b92f0fd5b16954e0cbf7785f5b9721635d1162a
Chaia81 <mariawagner@mariawagner.dk>
2020-10-08 09:55:50 +0200

    Initial commit.

---
+++ src/main/java/com/example/demo/CpHspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpHspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CpHspringApplication.class, args);
+    }
+
+}
